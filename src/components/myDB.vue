0ff9e9ca6059588996b9d428de0632a5ea10e89b
laixinyi <laixinyi@cqu.edu.cn>
2021-04-14 19:44:53 +0800

    upload DB

--- src/components/myDB.vue
+++ src/components/myDB.vue
@@ -1,174 +1,122 @@
 <template>
   <el-container>
-    <div v-if="opt == 'DBList'">
-      <div @click="addDialogVisible=true" style="height: 200px; float: left">
-        <el-card class="box-card" shadow="never">
-          <i style="font-size: 20px; float: left; margin-top: 50px" class="el-icon-plus">
-            添加文本数据</i>
-        </el-card>
-      </div>
-      <div @click="DBDetail()">
-        <el-card
-          class="box-card"
-          v-for="project in projectList"
-          :key="project.project_id"
-          shadow="never"
-        >
-          <div slot="header" class="clearfix">
-            <span>{{ project.project_name }}</span>
-          </div>
-        </el-card>
-      </div>
-      <el-dialog
-        title="添加文本数据"
-        :visible.sync="addDialogVisible"
-      >
-        <el-form :model="newDB">
-          <el-form-item label="添加方式：">
-            <el-radio
-              v-model="method"
-              label="DB"
-            >
-              DB
-            </el-radio>
-            <el-radio
-              v-model="method"
-              label="File"
-            >
-              File
-            </el-radio>
-          </el-form-item>
-          <div v-if="method=='DB'">
-            <el-form-item
-              label="数据名称"
-              :label-width="formLabelWidth"
-            >
-              <el-input
-                v-model="newDB.name"
-                autocomplete="off"
-              />
-            </el-form-item>
-            <el-form-item
-              label="数据类型"
-              :label-width="formLabelWidth"
-            >
-              <el-radio-group v-model="newDB.data_type">
-                <el-radio label="Text">
-                  Text
-                </el-radio>
-                <el-radio label="MySQL">
-                  MySQL
-                </el-radio>
-                <el-radio label="MongoDB">
-                  MongoDB
-                </el-radio>
-              </el-radio-group>
-            </el-form-item>
-            <el-form-item
-              label="ip"
-              :label-width="formLabelWidth"
-            >
-              <el-input
-                v-model="newDB.ip"
-                autocomplete="off"
-              />
-            </el-form-item>
-            <el-form-item
-              label="port"
-              :label-width="formLabelWidth"
-            >
-              <el-input
-                v-model="newDB.port"
-                autocomplete="off"
-              />
-            </el-form-item>
-            <el-form-item
-              label="username"
-              :label-width="formLabelWidth"
-            >
-              <el-input
-                v-model="newDB.username"
-                autocomplete="off"
-              />
-            </el-form-item>
-            <el-form-item
-              label="password"
-              :label-width="formLabelWidth"
-            >
-              <el-input
-                v-model="newDB.password"
-                autocomplete="off"
-              />
-            </el-form-item>
-            <el-form-item
-              label="db"
-              :label-width="formLabelWidth"
-            >
-              <el-input
-                v-model="newDB.db"
-                autocomplete="off"
-              />
-            </el-form-item>
-          </div>
-
-          <div v-if="method=='File'">
-            <el-upload
-              class="upload-demo"
-              ref="upload"
-              action="https://jsonplaceholder.typicode.com/posts/"
-              :on-preview="handlePreview"
-              :on-remove="handleRemove"
-              :before-remove="beforeRemove"
-              multiple
-              :limit="3"
-              :on-exceed="handleExceed"
-              :file-list="fileList"
-              accept=".txt,.csv"
-              :show-file-list="true"
-            >
-              <el-button
-                slot="trigger"
-                size="small"
-                type="primary"
-              >
-                选取文件
-              </el-button>
-              <div
-                slot="tip"
-                class="el-upload__tip"
-              >
-                只能上传txt/csv文件
+    <div v-if="opt == 'raw_data_list'" style="width:100%">
+      <!-- 数据列表 -->
+      <el-tabs type="border-card">
+        <el-tab-pane label="数据列表">
+        <el-table :data="raw_data_list">
+          <el-table-column label="数据ID" width="180">
+            <template slot-scope="scope">
+              <span>{{ scope.row.data_id }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="数据名称" width="250">
+            <template slot-scope="scope">
+              <span><div style="font-weight:bold">{{ scope.row.data_name }}</div></span>
+            </template>
+          </el-table-column>
+          <el-table-column label="数据类型" width="180">
+            <template slot-scope="scope">
+              <el-tag v-if="scope.row.data_type=='0'" size="medium" type="info">Text</el-tag>
+              <el-tag v-if="scope.row.data_type=='1'" size="medium">MySQL</el-tag>
+              <el-tag v-if="scope.row.data_type=='2'" size="medium" type="warning">MongoDB</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="数据权限" width="180">
+            <template slot-scope="scope">
+              <div slot="reference" class="name-wrapper">
+                <el-tag v-if="scope.row.private==true" size="medium" type="danger">私有</el-tag>
+                <el-tag v-else size="medium" type="success">公开</el-tag>
               </div>
-            </el-upload>
-          </div>
-        </el-form>
-        <div
-          slot="footer"
-          class="dialog-footer"
-        >
-          <el-button @click="addDialogVisible = false">
-            取 消
-          </el-button>
-          <el-button type="primary">
-            确 定
-          </el-button>
-        </div>
-      </el-dialog>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" style="margin-left: 10px">
+            <template slot-scope="scope">
+              <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">查看编辑</el-button>
+              <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        </el-tab-pane>
+        <!-- 添加新的raw_data -->
+        <el-tab-pane label="导入数据">
+            <!-- 选择添加方式 -->
+            请选择导入方式：
+            <el-radio v-model="method" label="DB">DB</el-radio>
+            <el-radio v-model="method" label="File">File</el-radio>
+            <el-divider><i class="el-icon-receiving"></i></el-divider>
+            <!--添加DB-->
+            <el-form v-if="method=='DB'" :model="new_raw_data_DB" ref="new_raw_data_DB" style="margin-left:25%">
+              <el-form-item label="数据名称" :label-width="formLabelWidth" style="width:500px">
+                <el-input v-model="new_raw_data_DB.name" autocomplete="off"/>
+              </el-form-item>
+              <el-form-item label="是否设置私有" :label-width="formLabelWidth" style="width:500px">
+                <el-radio-group v-model="new_raw_data_DB.private">
+                  <el-radio label="true">是</el-radio>
+                  <el-radio label="false">否</el-radio>
+                </el-radio-group>
+              </el-form-item>
+              <el-form-item label="数据类型" :label-width="formLabelWidth" style="width:500px">
+                <el-radio-group v-model="new_raw_data_DB.data_type">
+                  <el-radio label="0">Text</el-radio>
+                  <el-radio label="1">MySQL</el-radio>
+                  <el-radio label="2">MongoDB</el-radio>
+                </el-radio-group>
+              </el-form-item>
+              <el-form-item label="ip" :label-width="formLabelWidth" style="width:500px">
+                <el-input v-model="new_raw_data_DB.ip" autocomplete="off"/>
+              </el-form-item>
+              <el-form-item label="port" :label-width="formLabelWidth" style="width:500px">
+                <el-input v-model="new_raw_data_DB.port" autocomplete="off"/>
+              </el-form-item>
+              <el-form-item label="username" :label-width="formLabelWidth" style="width:500px">
+                <el-input v-model="new_raw_data_DB.username" autocomplete="off"/>
+              </el-form-item>
+              <el-form-item label="password" :label-width="formLabelWidth" style="width:500px">
+                <el-input v-model="new_raw_data_DB.password" autocomplete="off"/>
+              </el-form-item>
+              <el-form-item label="db" :label-width="formLabelWidth" style="width:500px">
+                <el-input v-model="new_raw_data_DB.db" autocomplete="off"/>
+              </el-form-item>
+              <el-form-item>
+                <el-button @click="add_raw_data_DB" type="primary" style="margin-left:13%;width:250px;">确 定</el-button>
+                <el-button @click="resetForm">重 置</el-button>               
+              </el-form-item>
+              
+            </el-form>
+            <!-- 添加文件 -->
+            <div v-if="method=='File'">
+              <el-upload
+                class="upload-demo"
+                ref="upload"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :on-preview="handlePreview"
+                :on-remove="handleRemove"
+                :before-remove="beforeRemove"
+                multiple
+                :limit="3"
+                :on-exceed="handleExceed"
+                :file-list="fileList"
+                accept=".txt,.csv"
+                :show-file-list="true"
+              >
+                <el-button slot="trigger" size="small" type="primary">
+                  选取文件
+                </el-button>
+                <div slot="tip" class="el-upload__tip">
+                  只能上传txt/csv文件
+                </div>
+              </el-upload>
+              <el-button>取 消</el-button>
+              <el-button type="primary">确 定</el-button>
+            </div>
+        </el-tab-pane>
+      </el-tabs>
     </div>
 
-    <div
-      v-if="opt == 'DBDetail'"
-      style="width: 100%; height: 100px; background: white"
-    >
-      <el-card
-        style="width: 100%; height: 65px; background-color: #fff"
-        shadow="never"
-      >
-        <el-page-header
-          @back="goBack()"
-          :content="project_name + ' DB详情'"
-          style="float: left"
-        />
+    <div v-if="opt == 'raw_data_detail'" style="width: 100%; height: 100px; background: white">
+      <el-card style="width: 100%; height: 65px; background-color: #fff" shadow="never">
+        <el-page-header @back="goBack()" :content="project_name + ' DB详情'" style="float: left"/>
       </el-card>
       <div style="width: 100%; margin-top: 5px" />
     </div>
@@ -177,27 +125,28 @@
 
 <script>
 export default {
+  inject:['reload'],
   created() {
-    this.getProjectList();
+    this.get_raw_data_list();
   },
   data() {
     return {
-      projectList: [],
+      raw_data_list: [],
       addDialogVisible: false,
       formLabelWidth: '120px',
-      opt: 'DBList',
-      project_id: 0,
-      project_name: '',
+      opt: 'raw_data_list',
+      raw_data_id: 0,
+      raw_data_name: '',
       method:'DB',
-      newDB:{
-        'name':'',
-        'data_type':'',
-        'private':true,
-        'ip':'',
-        'port':'',
-        'username':'',
-        'password':'',
-        'db':''
+      new_raw_data_DB:{
+        "name":'',
+        "data_type":'',
+        "private":true,
+        "ip":'',
+        "port":'',
+        "username":'',
+        "password":'',
+        "db":''
       },
       form:{
         dataType:'DB',
@@ -211,15 +160,46 @@ export default {
     };
   },
   methods: {
-    async getProjectList() {
-      const res = await this.$http.get('list_projects');
-      this.projectList = res.data.data;
+    async get_raw_data_list() {
+      const {data: res} = await this.$http.get('raw_data');
+      this.raw_data_list = res.data;
     },
     // 详情界面返回函数
     goBack() {
       this.opt = 'projectList';
     },
-    DBDetail(){
+    // 导入raw_data
+    async add_raw_data_DB(){
+      console.log(this.new_raw_data_DB)
+      this.new_raw_data_DB.data_type=parseInt(this.new_raw_data_DB.data_type)
+      if(this.new_raw_data_DB.private=='true')
+        this.new_raw_data_DB.private=true;
+      else
+        this.new_raw_data_DB.private=false;
+      const {data: res} = await this.$http.post('raw_data',this.new_raw_data_DB);
+      console.log(res)
+      if (res.errno==="0"){
+        this.$message({
+            showClose: true,
+            message: '导入成功！',
+            type: 'success'
+        });
+        this.reload();
+      }
+      else{
+          this.$message({
+              showClose: true,
+              message: '导入失败！',
+              type: 'error'
+          });
+      }    
+    },
+    // 重置导入数据表格
+    resetForm() {
+        this.$refs.new_raw_data_DB.resetFields();
+        console.log("1")
+    },
+    raw_data_detail(){
       this.opt = 'DBDetail';
     },
     handleRemove(file, fileList) {
